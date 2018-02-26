dfcd59542fb181bf5b2c74c4d02ae26728cf0993
Kelly Jackson <hi@kellyjackson.io>
2018-02-26 15:11:17 -0600

    First commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const   express = require('express'),
+        hbs     = require('hbs'),
+        colors  = require('colors'),
+        fs      = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append server.log');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance', {
+//         pageTitle: 'Maintenance Page'
+//     });
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamer', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        pageTitle: 'Home Page',
+        welcomeMessage: "Yo Bics, What's Good" 
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        error: 'Unable to fulfill request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('server listening!!!!!!!!!!!!!!!!!!!!!!!!'.rainbow);
+});
